9dc06d7292a2b124d33468e9e4930e5b9418dd2e
evelynmaianhtran <140688900+evelynmaianhtran@users.noreply.github.com>
2025-06-13 15:25:26 +1000

    Update doctors_office.cpp

--- doctors_office.cpp
+++ doctors_office.cpp
@@ -1,10 +1,7 @@
 /*
  * doctors_office.cpp
- * Written by : Mai Anh Tran (c3427150)
- * Modified : 12/04/2024
  *
  * This class represents a doctor's office in a queuing system, mainly a wrapper for a vector of doctor objects.
- * This file should be used in conjunction with Assignment 2 for SENG1120/SENG6120
  */
 
 #include "doctors_office.h"
